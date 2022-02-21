1e7ea3312fa0a7ada34d27a81d71ebe01c53ac66
sTunahan <st.ustuntepe@gmail.com>
2022-02-21 13:51:09 +0300

    Initial Commit

---
+++ Number Prediction APP/Number_Prediction_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Number_Prediction_APPApp.swift
+//  Number Prediction APP
+//
+//  Created by asuftech on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Number_Prediction_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
